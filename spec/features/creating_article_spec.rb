a260a3d78d1ddc22dd3fa76859055ee63dab0ddf
Nicholas Utikal <nicholasutikal@gmail.com>
2018-04-21 11:26:59 +0200

    implement displaying a message when no articles present

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature "Creating Articles" do
     click_button "Create Article"
 
     expect(page).to have_content("Article has not been created")
-    expect(page).to have_content("Title cant be blank")
-    expect(page).to have_content("Body cant be blank")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
   end
 end
